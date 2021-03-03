476bf9313acb91b2a358ac5db678a20d4a556696
Robby Novianto <robbyhahaha@gmail.com>
2021-03-04 03:54:42 +0700

    Menambahkan semua file Wed Dev Latihan

---
+++ resources/views/mahasiswa.blade.php
@@ -0,0 +1,13 @@
+@extends('layout/main')
+
+@section('title', 'Mahasiswa')
+
+@section('container')
+      <div class="container">
+          <div class="row">
+              <div class="col-4">
+                  <h1 class="mt-2">Mahasiswa</h1>
+              </div>
+          </div>
+      </div>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PagesController;
+use App\Http\Controllers\MahasiswaController;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('index');
+// });
+
+// Route::get('/about', function () {
+//     $nama = 'Robby Novianto';
+//     return view('about', ['nama' => $nama]);
+// });
+
+// Route::get('/mahasiswa', function () {
+//     return view('mahasiswa');
+// });
+
+Route::get('/', [PagesController::class, 'home']);
+Route::get('/about', [PagesController::class, 'about']);
+
+Route::get('/mahasiswa', [MahasiswaController::class, 'index']);
